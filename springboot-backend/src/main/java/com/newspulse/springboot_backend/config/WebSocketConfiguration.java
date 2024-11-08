1c68c3cc46f96729f1e52a96fe32a1a6d58d1744
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2024-11-08 18:48:18 +0530

    web socket configuration

---
+++ springboot-backend/src/main/java/com/newspulse/springboot_backend/config/WebSocketConfiguration.java
@@ -0,0 +1,22 @@
+package com.newspulse.springboot_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/chat");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+    
+}
